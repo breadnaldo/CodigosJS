19d06b58857c17141bcbbb43208e4ee74fce0d7b
João Victor de Medeiros <medeirosjoaoadv@gmail.com>
2021-04-10 12:19:00 -0300

    Atualização.

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,5 +1,80 @@
 
-var nome = "Joao Victor";
+/*function soma (n1,n2) {
+return n1+n2 ;    
+}
+alert(soma (5,10));
+*/
+
+function validaidade(idade){
+    var validar;    
+    if (idade >= 18){
+        validar = true 
+    }else{
+        validar = false 
+    }
+    return validar; 
+} 
+
+var idade = prompt ("Qual sua idade ? ");
+console.log(validaidade (idade));
+
+
+
+
+/*
+var d = new Date ();
+alert (d.getMinutes());
+alert (d.getDay());
+alert (d.getMonth());
+alert (d.getHours());
+*/
+
+/*
+var count;
+for(count=0 ; count <= 5 ; count++){
+    alert (count);
+};
+*/
+
+/*var count = 0;
+while (count <=5){
+    console.log (count);
+    count = count + 1;
+    alert (count);
+    //ou
+    //count++
+}
+*/
+
+/*
+var idade = prompt ("qual sua idade ?")
+if (idade >= 18){
+    alert ("maior de idade")
+}else{
+    alert ("menor de idade")
+};
+*/
+
+/*var frutas = [{nome: "maça", cor:"vermelha"}, {nome: "uva", cor:"roxa"}]
+console.log (frutas);
+alert (frutas [1].nome);*/
+
+//var fruta = {nome:"maça", cor:"vermelha"}
+//console.log (fruta.nome)
+//alert (fruta.cor)
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop ();
+
+//console.log(lista[0]);
+//console.log(lista.toString()[0])
+//console.log (lista.join(" - "))
+
+//console.log (lista.length);
+//console.log(lista.reverse());
+
+/*var nome = "Joao Victor";
 var n1 = 5;
 var n2 = 3;
 var frase = "Japão é o melhor time do mundo";
@@ -8,6 +83,7 @@ var frase = "Japão é o melhor time do mundo";
 console.log (nome);
 console.log (n1 * n2);
 console.log (frase.toLowerCase());
-//alert (frase.replace ("Japão", "Brasil"));
+//alert (frase.replace ("Japão", "Brasil"));*/
+
 
 
